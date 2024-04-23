f326263c57f718ef0c866316792cfa797f2f4021
sj <sunje1013@gmail.com>
2024-04-23 09:32:17 +0900

    javascript class

---
+++ 0403/하림펫푸드/header.php
@@ -0,0 +1,84 @@
+  <!-- 파비콘 입력 -->
+  <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+  <!-- css초기화 -->
+  <link rel="stylesheet" href="./css/reset.css" type="text/css">
+  <link rel="stylesheet" href="./css/common.css" type="text/css">
+  <!-- 아이콘폰트 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+  <!-- Link Swiper's CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+</head>
+<body>
+<form name="search" method="post" action="search">
+  <!-- 상단헤더영역시작 -->
+  <header>
+    <div class="h_top">
+      <h1><a href="index.html" title="메인페이지로 바로가기"><img src="./images/logo2.png" alt="상단로고"></a></h1>
+
+      <nav class="top_lnb">
+        <ul>
+          <li><a href="login.html" title="로그인">로그인</a></li>
+          <li><a href="join.html" title="회원가입">회원가입</a></li>
+          <li><a href="order.html" title="주문조회">주문조회</a></li>
+          <li><a href="cart.html" title="장바구니">장바구니</a></li>
+        </ul>
+      </nav>
+
+      <fieldset id="search_box">
+        <input type="search" id="search" placeholder="3월 마지막주 초특가 할인">
+        <label for="search"><i class="fas fa-search"></i></label>
+      </fieldset>
+    </div>
+    <!-- 헤더하단 (메인메뉴영역) -->
+    <div class="h_bottom">
+      <nav class="gnb">
+        <ul>
+          <li><a href="brand.html" title="브랜드">브랜드</a>
+            <ul class="sub">
+              <li><a href="brand.html" title="가장 맛있는 시간 30일">가장맛있는시간30일</a></li>
+              <li><a href="#" title="더리얼">더리얼</a></li>
+              <li><a href="#" title="밥이보약">밥이보약</a></li>
+            </ul>
+          </li>
+          <li><a href="deli.html" title="정기배송">정기배송</a>
+            <ul class="sub">
+              <li><a href="deli.html" title="정기배송">정기배송</a></li>
+              <li><a href="#" title="빠른배송">빠른배송</a></li>
+            </ul>
+          </li>
+          <li><a href="dog.html" title="DOG">DOG</a>
+            <ul class="sub">
+              <li><a href="dog.html" title="오늘생산&middot;배송">오늘생산 &middot; 배송</a></li>
+              <li><a href="#" title="브랜드별">브랜드별</a></li>
+              <li><a href="#" title="연령별">연령별</a></li>
+              <li><a href="#" title="유형별">유형별</a></li>
+              <li><a href="#" title="샘플신청">샘플신청</a></li>
+              <li><a href="#" title="상품후기">상품후기</a></li>
+            </ul>
+          </li>
+          <li><a href="cat.html" title="CAT">CAT</a>
+            <ul class="sub">
+              <li><a href="cat.html" title="오늘생산&middot;배송">오늘생산 &middot; 배송</a></li>
+              <li><a href="#" title="브랜드별">브랜드별</a></li>
+              <li><a href="#" title="연령별">연령별</a></li>
+              <li><a href="#" title="유형별">유형별</a></li>
+              <li><a href="#" title="샘플신청">샘플신청</a></li>
+              <li><a href="#" title="상품후기">상품후기</a></li>
+            </ul>
+          </li>
+          <li><a href="magiz.html" title="매거진">매거진</a></li>
+          <li><a href="story.html" title="우리이야기">우리이야기</a>
+            <ul class="sub">
+              <li><a href="story.html" title="우리이야기">우리이야기</a></li>
+              <li><a href="#" title="우리의약속">우리의약속</a></li>
+              <li><a href="#" title="100%휴먼그레이드">100%휴먼그레이드</a></li>
+              <li><a href="#" title="이노베이션">이노베이션</a></li>
+              <li><a href="#" title="제조시설">제조시설</a></li>
+              <li><a href="#" title="반려견동반">반려견동반</a></li>
+            </ul>
+          </li>
+          <li><a href="tour.html" title="투어신청">투어신청</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
---
+++ 0416/script/gallery.js
@@ -0,0 +1,70 @@
+//갤러리 스크립트
+
+//변수 페이지번호, 좌, 우버튼
+let n=1;
+let img_n = document.querySelectorAll('.list_photo > li');
+let l_btn = document.querySelectorAll('.fa-angle-left');
+let r_btn = document.querySelectorAll('.fa-angle-right');
+
+document.getElementById('img_num').innerHTML=n+'/17';
+
+// 썸네일 이미지 목록(li)태그 클릭시 해당 인덱스번호를 구하여 
+for(let i=0; i<img_n.length; i++){
+  img_n[i].addEventListener('click', function(){
+    // console.log(i+1);
+    n = i+1;
+
+    imgChange(n);
+  })
+}
+
+// 이미지 변경하기 위한 함수로 넘기기
+
+/*  사용자 정의함수 imgChange()
+  1. 숫자변경
+  2. 이미지변경
+*/
+
+function imgChange(){
+  console.log(n);
+
+  for(let j=0; j<img_n.length; j++){
+    img_n[j].style.border='none';
+    img_n[j].style.boxSizing = 'border-box';
+  }
+
+  img_n[n-1].style.border='3px solid #f00';
+  document.getElementById('img_num').innerHTML=n+'/17';
+  document.getElementById('main').src='./images01/photo'+n+'.jpg';
+}
+
+//ForEach함수는 배열에서만 사용가능한 메서드이다.
+/*
+문법
+  객체.forEach((매개변수, index)){
+    각각 실행할 함수 내용을 적는다.
+  }
+*/
+// 좌,우버튼 클릭시 n값을 imgChange함수에 넘기기
+l_btn.forEach((el)=>{
+  el.addEventListener('click', function(){
+    // alert('왼쪽버튼클릭');
+    if(n==1){
+      n=17;
+    }else{
+      n--;
+    }
+    imgChange(n); //숫자를 함수에 넘겨준다.
+  });
+});
+r_btn.forEach((el)=>{
+  el.addEventListener('click', function(){
+    // alert('오른쪽버튼클릭');
+    if(n==17){
+      n=1;
+    }else{
+      n++;
+    }
+    imgChange(n);
+  });
+});
